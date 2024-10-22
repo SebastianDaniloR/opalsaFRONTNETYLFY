c2b1b964885576d675ff2ee035145d8fb098aaf3
unknown <sebastiandanilo19@gmail.com>
2024-10-22 16:14:06 -0500

    s

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,12 +1,12 @@
 import axios from './axios';
 
-export const registerRequest = (user) => axios.post(`/register`, user, { withCredentials: true });
-export const loginRequest = (user) => axios.post(`/login`, user, { withCredentials: true });
-export const verifyTokenRequest = (token) => axios.get('/verify', {
+export const registerRequest = (user) => axios.post(`/api/register`, user, { withCredentials: true });
+export const loginRequest = (user) => axios.post(`/api/login`, user, { withCredentials: true });
+export const verifyTokenRequest = (token) => axios.get('/api/verify', {
   headers: {
     'Authorization': `Bearer ${token}`
   },
   withCredentials: true
 });
-export const updateUserRequest = (id, user) => axios.put(`/users/${id}`, user, { withCredentials: true });
-export const getUserDataRequest = (userId) => axios.get(`/users/${userId}`, { withCredentials: true });
+export const updateUserRequest = (id, user) => axios.put(`/api/users/${id}`, user, { withCredentials: true });
+export const getUserDataRequest = (userId) => axios.get(`/api/users/${userId}`, { withCredentials: true });
