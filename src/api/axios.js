d14dc05d0870a6ae67d8f9b54766dbd0a4ad246c
jhonrivera1100 <jhonrivera416@gmail.com>
2024-10-23 02:07:39 -0500

    dd

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 
+// Crea una instancia de Axios
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,  // Usar la variable de entorno para la URL base
-  withCredentials: true
+  withCredentials: true // Permitir que Axios envíe cookies con las solicitudes
+});
+
+// Interceptor para añadir el token en cada solicitud
+instance.interceptors.request.use((config) => {
+  const token = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('rw.authenticated='))
+    ?.split('=')[1]; // Extrae el token de la cookie
+
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`; // Añade el token a los headers
+  }
+
+  return config; // Devuelve la configuración modificada
+}, (error) => {
+  return Promise.reject(error); // Rechaza el error
 });
 
 export default instance;
